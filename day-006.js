571cf96ea9e71d24920a1480942b303675796d98
Changjun Yeo <lunaradio24@gmail.com>
2024-04-19 17:50:23 +0900

    codekata day 6 - addition of two integers

---
+++ day-006.js
@@ -0,0 +1,7 @@
+function addition(num1, num2) {
+  if (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000) {
+    return num1 + num2;
+  } else {
+    alert("Please put integers between -50000 and 50000");
+  }
+}
