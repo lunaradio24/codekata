788ae0dd6fbc455bca38f5ab45f5928f8db683b0
Changjun Yeo <lunaradio24@gmail.com>
2024-04-19 17:32:19 +0900

    codekata day 3 - quotient

---
+++ day-003.js
@@ -0,0 +1,7 @@
+function quotient(num1, num2) {
+  if (0 < num1 && num1 <= 100 && 0 < num2 && num2 <= 100) {
+    return parseInt(num1 / num2);
+  } else {
+    alert("Please put integers between 0 and 100");
+  }
+}
