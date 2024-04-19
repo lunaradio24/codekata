8063d638415922366962e698cfe5167d2b927d75
Changjun Yeo <lunaradio24@gmail.com>
2024-04-19 17:25:11 +0900

    codekata day 2 - multiplication

---
+++ day-002.js
@@ -0,0 +1,7 @@
+function multiplication(num1, num2) {
+  if (0 <= num1 && num1 <= 100 && 0 <= num2 && num2 <= 100) {
+    return num1 * num2;
+  } else {
+    alert("Please put integers between 0 and 100");
+  }
+}
