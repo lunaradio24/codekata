d9f82715cf819031dbfaaec86ad9cc5fd85f9385
Changjun Yeo <lunaradio24@gmail.com>
2024-04-19 17:47:25 +0900

    codekata day 5 - compare two integers

--- day-005.js
+++ day-005.js
@@ -1,3 +1,4 @@
+//compare two integers and return 1 if they are the same and -1 if they are not.
 function compare(num1, num2) {
   if (0 <= num1 && num1 <= 10000 && 0 <= num2 && num2 <= 10000) {
     if (num1 == num2) {
